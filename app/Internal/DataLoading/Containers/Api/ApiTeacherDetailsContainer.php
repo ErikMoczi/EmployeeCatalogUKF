7835faf5ebdab4df4564241a0a45178d9e9bcb35
ErikMoczi <ErikMoczi@users.noreply.github.com>
2017-11-05 08:12:48 +0100

    Add base structure for ApiTeacherDetailsContainer

---
+++ app/Internal/DataLoading/Containers/Api/ApiTeacherDetailsContainer.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Internal\DataLoading\Containsers\Api;
+
+use App\Internal\DataLoading\Containsers\Api\RawData\TeacherDetails;
+
+class ApiTeacherDetailsContainer
+{
+    /**
+     * @var array
+     */
+    private $teacherDetails;
+
+    public function __construct(array $teacherDetails)
+    {
+        $this->teacherDetails = $teacherDetails;
+    }
+
+    public function getTeacherDetails() : TeacherDetails
+    {
+        return $this->getTeacherDetails();
+    }
+}
