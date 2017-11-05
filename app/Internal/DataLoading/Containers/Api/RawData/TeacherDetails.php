3ccf51e4e633419fc5cbdd591896034d9091b3e5
ErikMoczi <ErikMoczi@users.noreply.github.com>
2017-11-05 12:54:19 +0100

    Update RawData model - teacherDetails

--- app/Internal/DataLoading/Containers/Api/RawData/TeacherDetails.php
+++ app/Internal/DataLoading/Containers/Api/RawData/TeacherDetails.php
@@ -9,16 +9,101 @@
 namespace App\Internal\DataLoading\Containsers\Api\RawData;
 
 
+/**
+ * Class TeacherDetails
+ * @package App\Internal\DataLoading\Containsers\Api\RawData
+ */
 class TeacherDetails
 {
     /**
-     * @var string
+     * @var Profile
      */
     private $profile;
 
+    /**
+     * @var \ArrayObject[Publication]
+     */
     private $publications;
 
+    /**
+     * @var \ArrayObject[Project]
+     */
     private $projects;
 
+    /**
+     * @var \ArrayObject[Activity]
+     */
     private $activities;
+
+    /**
+     * @return Profile
+     */
+    public function getProfile(): Profile
+    {
+        return $this->profile;
+    }
+
+    /**
+     * @param Profile $profile
+     * @return $this
+     */
+    public function setProfile(Profile $profile)
+    {
+        $this->profile = $profile;
+        return $this;
+    }
+
+    /**
+     * @return \ArrayObject[Publication]
+     */
+    public function getPublications(): \ArrayObject
+    {
+        return $this->publications;
+    }
+
+    /**
+     * @param \ArrayObject $publications
+     * @return $this
+     */
+    public function setPublications(\ArrayObject $publications)
+    {
+        $this->publications = $publications;
+        return $this;
+    }
+
+    /**
+     * @return \ArrayObject[Project]
+     */
+    public function getProjects(): \ArrayObject
+    {
+        return $this->projects;
+    }
+
+    /**
+     * @param \ArrayObject $projects
+     * @return $this
+     */
+    public function setProjects(\ArrayObject $projects)
+    {
+        $this->projects = $projects;
+        return $this;
+    }
+
+    /**
+     * @return \ArrayObject[Activity]
+     */
+    public function getActivities(): \ArrayObject
+    {
+        return $this->activities;
+    }
+
+    /**
+     * @param \ArrayObject $activities
+     * @return $this
+     */
+    public function setActivities(\ArrayObject $activities)
+    {
+        $this->activities = $activities;
+        return $this;
+    }
 }
