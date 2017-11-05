d5082672cf6b2ec19ecdd3a206d5fc5ed735c8ce
ErikMoczi <ErikMoczi@users.noreply.github.com>
2017-11-05 08:51:00 +0100

    RawData model - project

---
+++ app/Internal/DataLoading/Containers/Api/RawData/Project.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Erik
+ * Date: 4.11.2017
+ * Time: 22:30
+ */
+
+namespace App\Internal\DataLoading\Containsers\Api\RawData;
+
+
+/**
+ * Class Project
+ * @package App\Internal\DataLoading\Containsers\Api\RawData
+ */
+class Project
+{
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var int
+     */
+    private $yearFrom;
+
+    /**
+     * @var int
+     */
+    private $yearEnd;
+
+    /**
+     * @var string
+     */
+    private $regNumber;
+
+    /**
+     * @return string
+     */
+    public function getTitle() : string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     * @return $this
+     */
+    public function setTitle(string $title)
+    {
+        $this->title = $title;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getYearFrom() : int
+    {
+        return $this->yearFrom;
+    }
+
+    /**
+     * @param int $yearFrom
+     * @return $this
+     */
+    public function setYearFrom(int $yearFrom)
+    {
+        $this->yearFrom = $yearFrom;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getYearEnd() : int
+    {
+        return $this->yearEnd;
+    }
+
+    /**
+     * @param int $yearEnd
+     * @return $this
+     */
+    public function setYearEnd(int $yearEnd)
+    {
+        $this->yearEnd = $yearEnd;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getRegNumber() : string
+    {
+        return $this->regNumber;
+    }
+
+    /**
+     * @param string $regNumber
+     * @return $this
+     */
+    public function setRegNumber(string $regNumber)
+    {
+        $this->regNumber = $regNumber;
+        return $this;
+    }
+}
