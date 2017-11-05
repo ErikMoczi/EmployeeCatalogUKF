ef284c3a4c175964ca171f41f6b481a316315448
ErikMoczi <ErikMoczi@users.noreply.github.com>
2017-11-05 08:20:14 +0100

    RawData model - teacherDetails

---
+++ app/Internal/DataLoading/Containers/Api/RawData/TeacherDetails.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Erik
+ * Date: 4.11.2017
+ * Time: 22:21
+ */
+
+namespace App\Internal\DataLoading\Containsers\Api\RawData;
+
+
+class TeacherDetails
+{
+    /**
+     * @var string
+     */
+    private $profile;
+
+    private $publications;
+
+    private $projects;
+
+    private $activities;
+}
