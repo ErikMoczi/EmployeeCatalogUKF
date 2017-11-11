247fff173df37cecf8dd357b99f1c80e9a2186e6
ErikMoczi <ErikMoczi@users.noreply.github.com>
2017-11-11 14:17:32 +0100

    Add repositories for DataPumpBase

--- app/Internal/DataLoading/DataPumpBase.php
+++ app/Internal/DataLoading/DataPumpBase.php
@@ -10,6 +10,11 @@ use App\Internal\DataLoading\Containsers\Api\RawData\RDTeacherDetails;
 use App\Internal\DataLoading\Containsers\Url\ApiUrlTeacherContainer;
 use App\Internal\DataLoading\Containsers\Url\ApiUrlTeachersContainer;
 use App\Internal\DataLoading\Containsers\Url\IApiUrlContainer;
+use App\Repositories\DataLoading\ActivityRepository;
+use App\Repositories\DataLoading\EmployeeRepository;
+use App\Repositories\DataLoading\ProfileRepository;
+use App\Repositories\DataLoading\ProjectRepository;
+use App\Repositories\DataLoading\PublicationRepository;
 
 /**
  * Class DataPumpBase
@@ -23,12 +28,94 @@ abstract class DataPumpBase implements IDataPump
     private $apiCommunication;
 
     /**
+     * @var EmployeeRepository
+     */
+    private $employeeRepository;
+
+    /**
+     * @var ActivityRepository
+     */
+    private $activityRepository;
+
+    /**
+     * @var ProjectRepository
+     */
+    private $projectRepository;
+
+    /**
+     * @var PublicationRepository
+     */
+    private $publicationRepository;
+
+    /**
+     * @var ProfileRepository
+     */
+    private $profileRepository;
+
+    /**
+     * @return EmployeeRepository
+     */
+    public function getEmployeeRepository(): EmployeeRepository
+    {
+        return $this->employeeRepository;
+    }
+
+    /**
+     * @return ActivityRepository
+     */
+    public function getActivityRepository(): ActivityRepository
+    {
+        return $this->activityRepository;
+    }
+
+    /**
+     * @return ProjectRepository
+     */
+    public function getProjectRepository(): ProjectRepository
+    {
+        return $this->projectRepository;
+    }
+
+    /**
+     * @return PublicationRepository
+     */
+    public function getPublicationRepository(): PublicationRepository
+    {
+        return $this->publicationRepository;
+    }
+
+    /**
+     * @return ProfileRepository
+     */
+    public function getProfileRepository(): ProfileRepository
+    {
+        return $this->profileRepository;
+    }
+
+    /**
      * DataPumpBase constructor.
      * @param ApiCommunication $apiCommunication
+     * @param EmployeeRepository $employeeRepository
+     * @param ActivityRepository $activityRepository
+     * @param ProjectRepository $projectRepository
+     * @param PublicationRepository $publicationRepository
+     * @param ProfileRepository $profileRepository
      */
-    public function __construct(ApiCommunication $apiCommunication)
+    public function __construct(
+        ApiCommunication $apiCommunication,
+        EmployeeRepository $employeeRepository,
+        ActivityRepository $activityRepository,
+        ProjectRepository $projectRepository,
+        PublicationRepository $publicationRepository,
+    ProfileRepository $profileRepository
+    )
     {
         $this->apiCommunication = $apiCommunication;
+        $this->employeeRepository = $employeeRepository;
+        $this->activityRepository = $activityRepository;
+        $this->projectRepository = $projectRepository;
+        $this->publicationRepository = $publicationRepository;
+        $this->profileRepository = $profileRepository;
     }
 
     protected function getApiTeachers(): ApiTeachersContainer
