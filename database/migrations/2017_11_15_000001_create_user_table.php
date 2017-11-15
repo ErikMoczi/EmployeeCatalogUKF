8b7327f2246401ceaa31042fbf6a6e0146f08299
ErikMoczi <ErikMoczi@users.noreply.github.com>
2017-11-15 21:13:31 +0100

    Add migration for user

---
+++ database/migrations/2017_11_15_000001_create_user_table.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration
+{
+    /**
+     * Schema table name to migrate
+     * @var string
+     */
+    public $set_schema_table = 'user';
+
+    /**
+     * Run the migrations.
+     * @table user
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (Schema::hasTable($this->set_schema_table)) return;
+        Schema::create($this->set_schema_table, function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('name',255);
+            $table->string('email',255);
+            $table->string('password',255);
+            $table->unsignedInteger('employee_id')->nullable();
+            $table->boolean('admin_flag')->default(0);
+            $table->rememberToken();
+
+            $table->unique(["email"], 'email_UNIQUE');
+            $table->unique(["employee_id"], 'employee_id_UNIQUE');
+
+            $table->index(["employee_id"], 'fk_user_employee1_idx');
+
+            $table->foreign('employee_id', 'fk_user_employee1')
+                ->references('id')->on('employee')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists($this->set_schema_table);
+    }
+}
