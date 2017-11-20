b9443c9c446bd3b6d52334e188a465ced96219ea
ErikMoczi <ErikMoczi@users.noreply.github.com>
2017-11-20 21:08:00 +0100

    Update HomeController

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -15,6 +15,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('frontend.index')->with('content_header', 'Hello world');
+        return view('frontend.index');
     }
 }
