a69e6364d2809d0b1cb29cbdde1cae0c77ef18a2
ErikMoczi <ErikMoczi@users.noreply.github.com>
2017-11-20 21:08:13 +0100

    Add Login blade

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.master')
+
+@push('css')
+    <link rel="stylesheet" href="{{ asset('vendor/adminlte/plugins/iCheck/square/blue.css') }}">
+@endpush
+
+@push('js')
+    <script src="{{ asset('vendor/adminlte/plugins/iCheck/icheck.min.js') }}"></script>
+    <script>
+        $(function () {
+            $('input').iCheck({
+                checkboxClass: 'icheckbox_square-blue',
+                radioClass: 'iradio_square-blue',
+                increaseArea: '20%' // optional
+            });
+        });
+    </script>
+@endpush
+
+@section('body_class', 'login-page')
+
+@section('body')
+    <div class="login-box">
+        <div class="login-logo">
+            <a href="{{ route('home') }}"><b>UKF</b> katalóg</a>
+        </div>
+        <div class="login-box-body">
+            <p class="login-box-msg">Sign in</p>
+
+            <form action="{{ route('login') }}" method="post">
+                <div class="form-group has-feedback" {{ $errors->has('email') ? 'has-error' : '' }}>
+                    <input type="email" name="email" class="form-control" value="{{ old('email') }}" placeholder="Email"
+                           required autofocus>
+                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                    @if ($errors->has('email'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <div class="form-group has-feedback {{ $errors->has('password') ? 'has-error' : '' }}">
+                    <input type="password" name="password" class="form-control" placeholder="Password" required>
+                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                    @if ($errors->has('password'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <div class="row">
+                    <div class="col-xs-8">
+                        <div class="checkbox icheck">
+                            <label>
+                                <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember
+                                Me
+                            </label>
+                        </div>
+                    </div>
+                    <div class="col-xs-4">
+                        <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+@endsection
