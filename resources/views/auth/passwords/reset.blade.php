bb21bc71ac989293883ad3d54570f921e5e7b188
ErikMoczi <ErikMoczi@users.noreply.github.com>
2017-11-20 21:08:44 +0100

    Add resetPassword blade

---
+++ resources/views/auth/passwords/reset.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.master')
+
+@section('body_class', 'login-page')
+
+@section('body')
+    <div class="login-box">
+        <div class="login-logo">
+            <a href="{{ route('home') }}"><b>UKF</b> katalóg</a>
+        </div>
+        <div class="login-box-body">
+            <p class="login-box-msg">Reset Password</p>
+
+            <form action="{{ route('password.request') }}" method="post">
+                <input type="hidden" name="token" value="{{ $token }}">
+                <div class="form-group has-feedback" {{ $errors->has('email') ? 'has-error' : '' }}>
+                    <input type="email" name="email" class="form-control" value="{{ $email or old('email') }}"
+                           placeholder="Email" required autofocus>
+                    <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+                    @if ($errors->has('email'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('email') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <div class="form-group has-feedback {{ $errors->has('password') ? 'has-error' : '' }}">
+                    <input type="password" name="password" class="form-control" placeholder="Password" required>
+                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                    @if ($errors->has('password'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('password') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <div class="form-group has-feedback {{ $errors->has('password_confirmation') ? 'has-error' : '' }}">
+                    <input type="password" name="password_confirmation" class="form-control"
+                           placeholder="Confirm Password" required>
+                    <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+                    @if ($errors->has('password_confirmation'))
+                        <span class="help-block">
+                            <strong>{{ $errors->first('password_confirmation') }}</strong>
+                        </span>
+                    @endif
+                </div>
+                <button type="submit" class="btn btn-primary btn-block btn-flat">Reset Password</button>
+            </form>
+        </div>
+    </div>
+@endsection
