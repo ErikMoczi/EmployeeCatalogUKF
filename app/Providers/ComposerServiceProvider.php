633b49601383aedc53cfa5f8c55141d411dd45d7
ErikMoczi <ErikMoczi@users.noreply.github.com>
2017-11-22 13:47:49 +0100

    Inject user to view

---
+++ app/Providers/ComposerServiceProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Providers;
+
+use App\Http\Composers\GlobalComposer;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class ComposerServiceProvider
+ * @package App\Providers
+ */
+class ComposerServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        View::composer('*', GlobalComposer::class);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
