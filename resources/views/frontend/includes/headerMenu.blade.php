93b44d145a3386e87f193dbd85e5cd5c2965b805
ErikMoczi <ErikMoczi@users.noreply.github.com>
2017-11-26 20:53:10 +0100

    add support active class for header menu frontend

--- resources/views/frontend/includes/headerMenu.blade.php
+++ resources/views/frontend/includes/headerMenu.blade.php
@@ -1,21 +1,37 @@
 <!-- Collect the nav links, forms, and other content for toggling -->
 <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
     <ul class="nav navbar-nav">
-        <li class="active"><a href="{{ route('frontend.employee.index') }}">Employees</a></li>
-        <li class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">Publishing <span class="caret"></span></a>
+        <li class="{{ active_class(Active::checkUriPattern('employee*')) }}">
+            <a href="{{ route('frontend.employee.index') }}">Employees</a>
+        </li>
+        <li class="dropdown {{ active_class(Active::checkUriPattern(['publication*', 'project*', 'activity*'])) }}">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                Publishing <span class="caret"></span>
+            </a>
             <ul class="dropdown-menu" role="menu">
-                <li><a href="{{ route('frontend.publication.index') }}">Publications</a></li>
-                <li><a href="{{ route('frontend.project.index') }}">Projects</a></li>
-                <li><a href="{{ route('frontend.activity.index') }}">Activities</a></li>
+                <li class="{{ active_class(Active::checkUriPattern('publication*')) }}">
+                    <a href="{{ route('frontend.publication.index') }}">Publications</a>
+                </li>
+                <li class="{{ active_class(Active::checkUriPattern('project*')) }}">
+                    <a href="{{ route('frontend.project.index') }}">Projects</a>
+                </li>
+                <li class="{{ active_class(Active::checkUriPattern('activity*')) }}">
+                    <a href="{{ route('frontend.activity.index') }}">Activities</a>
+                </li>
             </ul>
         </li>
-        <li class="dropdown">
+        <li class="dropdown {{ active_class(Active::checkUriPattern(['position*', 'department*', 'faculty*'])) }}">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">Administration <span class="caret"></span></a>
             <ul class="dropdown-menu" role="menu">
-                <li><a href="{{ route('frontend.position.index') }}">Positions</a></li>
-                <li><a href="{{ route('frontend.department.index') }}">Departments</a></li>
-                <li><a href="{{ route('frontend.faculty.index') }}">Faculties</a></li>
+                <li class="{{ active_class(Active::checkUriPattern('position*')) }}">
+                    <a href="{{ route('frontend.position.index') }}">Positions</a>
+                </li>
+                <li class="{{ active_class(Active::checkUriPattern('department*')) }}">
+                    <a href="{{ route('frontend.department.index') }}">Departments</a>
+                </li>
+                <li class="{{ active_class(Active::checkUriPattern('faculty*')) }}">
+                    <a href="{{ route('frontend.faculty.index') }}">Faculties</a>
+                </li>
             </ul>
         </li>
     </ul>
