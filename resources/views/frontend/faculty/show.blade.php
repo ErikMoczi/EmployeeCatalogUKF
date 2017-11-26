3ffb7754685bf8830609ba2fccea0be591014fca
ErikMoczi <ErikMoczi@users.noreply.github.com>
2017-11-26 19:58:26 +0100

    Update frontend blade - Faculty

---
+++ resources/views/frontend/faculty/show.blade.php
@@ -0,0 +1,34 @@
+@extends('frontend.layouts.page')
+
+@section('content_header')
+    <h1>Faculty details</h1>
+@endsection
+
+@section('content')
+    <div class="box">
+        <div class="box-header with-border">
+            <h3 class="box-title">{{ $dataShow->name }}</h3>
+            <div class="box-tools pull-right">
+                @include('frontend.includes.previousNextRecord')
+            </div>
+        </div>
+        <div class="box-body">
+            <dl class="dl-horizontal">
+                <dt>Acronym</dt>
+                <dd>{{ $dataShow->acronym }}</dd>
+                <dt>Total departments</dt>
+                <dd>{{ count($dataShow->departments) }}</dd>
+                <dt>Total authors</dt>
+                <dd>{{ count($dataShow->employees) }}</dd>
+            </dl>
+        </div>
+        <!-- /.box-body -->
+        <div class="box-footer">
+            <ul>
+                @include('frontend.includes.boxFooter.user', ['employeesList' => $dataShow->employees])
+            </ul>
+        </div>
+        <!-- /.box-footer-->
+    </div>
+    <!-- /.box -->
+@endsection
