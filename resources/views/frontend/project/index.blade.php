5f89a9466f5fe6faec0bedd2a0e341108232d249
ErikMoczi <ErikMoczi@users.noreply.github.com>
2017-11-26 19:58:40 +0100

    Update frontend blade - Project

--- resources/views/frontend/project/index.blade.php
+++ resources/views/frontend/project/index.blade.php
@@ -4,75 +4,10 @@
     <h1>Projects</h1>
 @endsection
 
-@push('js')
-    <script>
-        $(function () {
-            $('#myTable').DataTable({
-                'paging': true,
-                'lengthChange': true,
-                'searching': true,
-                'ordering': true,
-                'info': true,
-                'autoWidth': true
-            })
-        })
-    </script>
-@endpush
-
 @section('content')
     <div class="row">
         <div class="col-xs-12">
-            <div class="box">
-                <div class="box-header">
-                    <h3 class="box-title">List of projects</h3>
-                </div>
-                <!-- /.box-header -->
-                <div class="box-body table-responsive">
-                    <table id="myTable" class="table table-hover table-fixed dataTable">
-                        <thead class="content-headerFixed">
-                        <tr>
-                            <th style="width: 10px;" class="text-center">#</th>
-                            <th class="text-left">Title</th>
-                            <th class="text-center">Year from</th>
-                            <th class="text-center">Year to</th>
-                            <th style="width: 15px;" class="text-center">Authors count</th>
-                            <th class="text-center">Actions</th>
-                        </tr>
-                        </thead>
-                        @foreach($tableData as $item)
-                            <tr>
-                                <td class="text-center">
-                                    {{ $loop->iteration }}
-                                </td>
-                                <td class="text-left">
-                                    {{ $item->title }}
-                                </td>
-                                <td class="text-middle">
-                                    {{ $item->year_from }}
-                                </td>
-                                <td class="text-middle">
-                                    {{ $item->year_to }}
-                                </td>
-                                <td class="text-center">
-                                    <span class="label label-danger">
-                                    {{ $item->employees_count }}
-                                    </span>
-                                </td>
-                                <td class="text-center">
-                                    <a href="{{ route('frontend.project.show', $item->id) }}"
-                                       class="btn btn-info btn-xs">
-                                        Detail
-                                    </a>
-                                </td>
-                            </tr>
-                        @endforeach
-                        <tbody>
-                        </tbody>
-                    </table>
-                </div>
-                <!-- /.box-body -->
-            </div>
-            <!-- /.box -->
+            @include('includes.datatable', ['dataTableBoxHeader' => 'List of projects'])
         </div>
     </div>
 @endsection
--- resources/views/frontend/project/show.blade.php
+++ resources/views/frontend/project/show.blade.php
@@ -1,13 +1,13 @@
 @extends('frontend.layouts.page')
 
 @section('content_header')
-    <h1>Project</h1>
+    <h1>Project details</h1>
 @endsection
 
 @section('content')
     <div class="box">
         <div class="box-header with-border">
-            <h3 class="box-title">{{ $project->title }}</h3>
+            <h3 class="box-title">{{ $dataShow->title }}</h3>
             <div class="box-tools pull-right">
                 @include('frontend.includes.previousNextRecord')
             </div>
@@ -15,25 +15,19 @@
         <div class="box-body">
             <dl class="dl-horizontal">
                 <dt>Year from</dt>
-                <dd>{{ $project->year_from }}</dd>
+                <dd>{{ $dataShow->year_from }}</dd>
                 <dt>Year to</dt>
-                <dd>{{ $project->year_to }}</dd>
+                <dd>{{ $dataShow->year_to }}</dd>
                 <dt>Registration number</dt>
-                <dd>{{ $project->reg_number }}</dd>
+                <dd>{{ $dataShow->reg_number }}</dd>
                 <dt>Total authors</dt>
-                <dd>{{ count($employees) }}</dd>
+                <dd>{{ count($dataShow->employees) }}</dd>
             </dl>
         </div>
         <!-- /.box-body -->
         <div class="box-footer">
             <ul>
-                @foreach($employees as $employee)
-                    <li>
-                        <a href="{{ route('frontend.employee.show', $employee->id) }}" class="link-black text-sm">
-                            <i class="fa fa-genderless margin-r-5"></i>{{ $employee->full_name }}
-                        </a>
-                    </li>
-                @endforeach
+                @include('frontend.includes.boxFooter.user', ['employeesList' => $dataShow->employees])
             </ul>
         </div>
         <!-- /.box-footer-->
