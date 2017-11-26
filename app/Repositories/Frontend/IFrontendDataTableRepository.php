bcc7987e46d93ee042cd347188fa2cdf17dded43
ErikMoczi <ErikMoczi@users.noreply.github.com>
2017-11-26 19:50:41 +0100

    Interface for frontendDataTables repository

---
+++ app/Repositories/Frontend/IFrontendDataTableRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Repositories\Frontend;
+
+
+use App\Repositories\IDataTableRepository;
+
+/**
+ * Interface IFrontendDataTableRepository
+ * @package App\Repositories\Frontend
+ */
+interface IFrontendDataTableRepository extends IDataTableRepository
+{
+
+}
