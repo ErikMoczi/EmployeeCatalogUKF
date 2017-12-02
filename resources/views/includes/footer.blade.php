9025fbeb06f8fa4424180e5071b2ad6cf42996a9
ErikMoczi <ErikMoczi@users.noreply.github.com>
2017-12-02 10:03:21 +0100

    Update author of project

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,7 +3,7 @@
         <div class="pull-right hidden-xs">
             <b>Version</b> 0.1 Beta
         </div>
-        <strong>Copyright &copy; 2017 <a href="#">Crazy monkeys</a>.</strong> All rights
+        <strong>Copyright &copy; 2017 <a href="https://github.com/ErikMoczi">Erik Móczi</a>.</strong> All rights
         reserved.
     </div>
     <!-- /.container -->
