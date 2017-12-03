41e39878bf31436b33d249a47fec7faace1c2cc2
ErikMoczi <ErikMoczi@users.noreply.github.com>
2017-12-03 21:12:39 +0100

    Update dashboard backend

--- resources/views/backend/dashboard.blade.php
+++ resources/views/backend/dashboard.blade.php
@@ -1,9 +1,14 @@
 @extends('backend.layouts.page')
 
 @section('content_header')
-    <h1>Welcome</h1>
+    <h1>Welcome admin</h1>
 @endsection
 
 @section('content')
-    Auth {{ auth()->user()->admin_flag }}
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <img class="img-responsive pad" src="http://rs827.pbsrc.com/albums/zz199/Reycom394/Emoticons/admin.gif~c200"
+                 alt="admin">
+        </div>
+    </div>
 @endsection
