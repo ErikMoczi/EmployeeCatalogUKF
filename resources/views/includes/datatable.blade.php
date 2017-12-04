ca82fff225c03fcd3d04f5d140de965e8a23e95b
ErikMoczi <ErikMoczi@users.noreply.github.com>
2017-12-04 16:12:09 +0100

    Add for dataTables box-tools

--- resources/views/includes/datatable.blade.php
+++ resources/views/includes/datatable.blade.php
@@ -5,6 +5,9 @@
 <div class="box">
     <div class="box-header with-border">
         <h3 class="box-title">{{ $dataTableBoxHeader }}</h3>
+        <div class="box-tools pull-right">
+            @yield('dataTable-tools')
+        </div>
     </div>
     <!-- /.box-header -->
     <div class="box-body table-responsive">
