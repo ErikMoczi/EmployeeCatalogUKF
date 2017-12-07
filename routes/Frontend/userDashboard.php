19bc74110f7b3b857ca266c6848e20de03ded13c
ErikMoczi <ErikMoczi@users.noreply.github.com>
2017-12-07 10:56:13 +0100

    Code refactor

--- routes/Frontend/userDashboard.php
+++ routes/Frontend/userDashboard.php
@@ -1,12 +1,8 @@
 <?php
 
-Route::group(['middleware' => ['auth']], function () {
+Route::group(['middleware' => 'auth'], function () {
     Route::group(['namespace' => 'User', 'as' => 'user.'], function () {
-        /*
-         * User Dashboard Specific
-         */
         Route::get('dashboard', 'DashboardController@index')->name('dashboard');
-
         Route::get('account', 'AccountController@index')->name('account');
         Route::patch('profile/update', 'ProfileController@update')->name('profile.update');
     });
